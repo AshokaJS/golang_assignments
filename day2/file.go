0a652501e79f4eeec0664719c1154810b0134c9e
Ashok Choudhary <ashok.choudhary@joshsoftware.com>
2025-01-14 17:01:38 +0530

    worked on assignment of day2 (roman to integer)

---
+++ day2/file.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var roman string
+	_, err := fmt.Scanln(&roman)
+	if err != nil {
+		fmt.Printf("error while reading input, err: %s", err)
+	}
+
+	m := make(map[byte]int)
+
+	m['I'] = 1
+	m['V'] = 5
+	m['X'] = 10
+	m['L'] = 50
+	m['C'] = 100
+	m['D'] = 500
+	m['M'] = 1000
+
+	l := len(roman)
+
+	var digit int
+	digit = 0
+
+	var i, j int
+	for i = 0; i < l-1; i++ {
+		j = i + 1
+
+		if m[roman[i]] < m[roman[j]] {
+			digit += (m[roman[j]] - m[roman[i]])
+			i++
+		} else {
+			digit += m[roman[i]]
+			if j+1 >= l {
+				digit += m[roman[j]]
+			}
+		}
+
+	}
+
+	fmt.Println(digit)
+
+}
